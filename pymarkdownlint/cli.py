d9611da0ea8e88272bd70b29eb2b3bca8451c66e
Joris Roovers <jroovers@cisco.com>
2015-07-18 13:00:43 +0200

    Added CLI support using click
    Additionally:
     - coverage now ignores virtualenv
     - console entry point 'markdownlint'

--- pymarkdownlint/cli.py
+++ pymarkdownlint/cli.py
@@ -1,6 +1,11 @@
-def get_foo():
-    return "foo"
+import click
+
+
+@click.command()
+def cli():
+    """ Markdown lint tool, checks your markdown for styling issues """
+    click.echo("foo")
 
 
 if __name__ == "__main__":
-    print get_foo()
+    cli()
--- pymarkdownlint/tests/test_cli.py
+++ pymarkdownlint/tests/test_cli.py
@@ -2,7 +2,11 @@ from pymarkdownlint.tests.base import BaseTestCase
 
 from pymarkdownlint import cli
 
+from click.testing import CliRunner
+
 
 class CLITests(BaseTestCase):
     def test_cli(self):
-        self.assertEqual(cli.get_foo(), "foo")
+        runner = CliRunner()
+        result = runner.invoke(cli.cli)
+        self.assertEqual(result.output, "foo\n")
